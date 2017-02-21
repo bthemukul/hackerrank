2eaff6a58022d936a2ab009d96816e488e5a1517
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-02-22 02:05:02 +0530

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -1,3 +1,32 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+// Reads complete line from STDIN
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+
+// Code starts here 
+
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+
 
     var j ;
     var e ;
