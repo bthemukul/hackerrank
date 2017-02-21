8bd23d98181ba972d23ffd87e70c55d1262b09aa
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-02-22 02:04:07 +0530

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+
+    var j ;
+    var e ;
+    var t ;
+    
+        j = parseInt(readLine());    
+        e = parseInt(readLine());
+        t = readLine();
+
+            console.log(i+j);
+            console.log((d+e).toFixed(1));
+            var hola = s+t ;
+            console.log(hola);
+                
