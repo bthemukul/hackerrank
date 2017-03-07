cb02dbe5bf3d481d95bfacdd9bc5b03625e622ee
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-03-08 00:49:02 +0530

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -42,6 +42,30 @@ function main() {
             console.log(hola);
                 
     
+    //C++ Program for the same !
+    /* 
+    // Declare second integer, double, and String variables.
+    int j ;
+    double e ;
+    string t ;
+    // Read and save an integer, double, and String to your variables.
+       cin >> j >> e ;
+        getline(cin >> ws , t);
+
+    // Print the sum of both integer variables on a new line.
+           
+                cout << (i+j) << "\n";
+            
+    // Print the sum of the double variables on a new line.
+            double f = d+e ;
+               cout << fixed << setprecision(1) << f << endl ;
+
+    // Concatenate and print the String variables on a new line
+                cout << (s+t);
+                
+    // The 's' variable above should be printed first.
+    
+    */
     
     
     
