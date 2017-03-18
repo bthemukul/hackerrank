cc38603c51d663df9aa46a405558dcb4d5bbcf2e
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-03-18 18:00:57 +0530

    Create  CandyReplenishingRobot.cpp

---
+++ CandyReplenishingRobot.cpp
@@ -0,0 +1,33 @@
+//Week 30 Code Competition 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int t;
+    cin >> n >> t;
+    vector<int> c(t);
+    for(int i = 0; i < t; i++){
+       cin >> c[i];
+    }
+    // your code goes here
+    
+    int m , count = 0;
+    int x = n ;
+    
+    for (int i = 0; i< (t-1); i++ ){
+        
+        x= x- c[i] ;
+        if(x<5){
+            m = n-x;
+            x+=m;
+            count +=m;
+            
+        }
+        
+    }
+    cout << count << endl ;
+    return 0;
+}
