79a3dc13b624aa7aaf05575bab9e52aa33cc499b
Mukul Bisen <bthemukul@users.noreply.github.com>
2017-03-27 00:27:53 +0530

    Create Cat&aMouse.cpp

---
+++ Cat&aMouse.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int x;
+        int y;
+        int z;
+        cin >> x >> y >> z;
+        
+        int ca = abs(z-x);
+        int cb = abs(z-y);
+        
+        if(ca>cb){cout << "Cat B" << endl ;}
+        if(ca<cb){cout << "Cat A" << endl ;}
+        if(ca==cb){cout << "Mouse C" << endl ;}
+        
+    }
+    return 0;
+}
